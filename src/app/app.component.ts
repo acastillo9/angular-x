63ca1094ebf69bef15ae145fd0342419b268615d
Andres Castillo <info@acastillo.dev>
2024-06-25 21:04:44 -0500

    Initial commit, add timeline component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TimelineComponent } from './views/timeline/timeline.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TimelineComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/views/timeline/timeline.component.ts
@@ -0,0 +1,12 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-timeline',
+  standalone: true,
+  imports: [],
+  templateUrl: './timeline.component.html',
+  styleUrl: './timeline.component.scss'
+})
+export class TimelineComponent {
+
+}
