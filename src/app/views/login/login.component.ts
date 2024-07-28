c5918e0b065e61c430d3c715733209f00b15c554
Andres Castillo <info@acastillo.dev>
2024-07-28 10:02:23 -0500

    improve login view styles

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 import { Router, RouterModule } from '@angular/router';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { faBrandXTwitter } from '@ng-icons/font-awesome/brands';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule, RouterModule],
+  imports: [FormsModule, RouterModule, NgIconComponent],
+  providers: [provideIcons({ faBrandXTwitter })],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
